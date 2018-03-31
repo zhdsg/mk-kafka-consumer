ac6f95134e24c1fcfa2d3787cc0989b47dfac2f3
Raven Zuo <raven@uuke.co>
2018-03-31 17:13:59 +0800

    initial commit with word count sample code

---
+++ build.sbt
@@ -0,0 +1,20 @@
+name := "mk-kafka-consumer"
+
+version := "1.0"
+
+scalaVersion := "2.11.12"
+
+val sparkVersion = "2.2.0"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion % "provided",
+  "org.apache.spark" %% "spark-streaming" % sparkVersion % "provided",
+  "org.apache.spark" %% "spark-streaming-kafka-0-10" % sparkVersion
+)
+
+mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
+{
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
+}
---
+++ src/main/scala/MKKafkaConsumer.scala
@@ -0,0 +1,52 @@
+/**
+  * Created by raven on 29/03/2018.
+  */
+
+import kafka.log.Log
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.internal.Logging
+
+object MKKafkaConsumer extends Logging {
+  def main(args: Array[String]) {
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "10.10.100.11:2181",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "test-consumer-group",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+
+    val topics = Array("useraction")
+
+    // Create context with 2 second batch interval
+    val sparkConf = new SparkConf()
+      .setAppName("MKKafkaConsumer")
+      .setMaster("local[3]")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+
+    // Create direct kafka stream with brokers and topics
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+//    // Get the lines, split them into words, count the words and print
+    val lines = stream.map(_.value())
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
+    wordCounts.print()
+
+    logInfo("start the computation...")
+    ssc.start()
+    ssc.awaitTermination()
+    logInfo("computation done!")
+  }
+}
